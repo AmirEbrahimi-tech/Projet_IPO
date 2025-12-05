3acb240f1ac88a1a58d2a9631da678150e34b520
Barbouille196 <ygharbi196@gmai.com>
2025-12-05 18:16:52 +0100

    Classe Void

--- CaseTraversable.java
+++ CaseTraversable.java
@@ -1,3 +1,10 @@
 public class CaseTraversable extends Case {
-    
+    protected Entite contenu;
+    public CaseTraversable(int ligne, int colonne){
+        super(ligne,colonne);
+        this.contenu = new Void();
+    }
+    public boolean estLibre(){
+        return this.contenu instanceof Void;    
+    }
 }
