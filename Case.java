598c37dcb7e6de7b084f50ee1e2b3016d62ff17c
Barbouille196 <ygharbi196@gmai.com>
2025-12-05 17:47:44 +0100

    faké

--- Case.java
+++ Case.java
@@ -1,6 +1,8 @@
-public class Case{
+public abstract class Case{
     private final int ligne,colonne;
     public Case(int ligne, int colonne){
         this.ligne=ligne;this.colonne=colonne;
     }
+
+    public abstract boolean estLibre();
 }
---
+++ CaseTraversable.java
@@ -0,0 +1,3 @@
+public class CaseTraversable extends Case {
+    
+}
