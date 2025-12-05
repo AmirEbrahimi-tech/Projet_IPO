07daedcfa4c89067b266dc5cb17e6337dafcaa87
Amir Ebrahimi <amir.ebrahimi.tech@gmail.com>
2025-12-05 18:37:19 +0100

    add documentation

--- Case.java
+++ Case.java
@@ -1,10 +1,14 @@
 // la classe abstrait Case
 public abstract class Case{
+    // les attributs
     private final int ligne,colonne;
+
+    // le constructeur
     public Case(int ligne, int colonne){
         this.ligne=ligne;
         this.colonne=colonne;
     }
 
+    // la méthode astrait estLibre
     public abstract boolean estLibre();
 }
--- CaseTraversable.java
+++ CaseTraversable.java
@@ -1,9 +1,15 @@
+// la classe CaseTraversable
 public class CaseTraversable extends Case {
+    // les attributs
     protected Entite contenu;
+
+    // le constructeur
     public CaseTraversable(int ligne, int colonne){
         super(ligne,colonne);
         this.contenu = new Void();
     }
+
+    // la méthode estLibre
     public boolean estLibre(){
         return this.contenu instanceof Void;    
     }
