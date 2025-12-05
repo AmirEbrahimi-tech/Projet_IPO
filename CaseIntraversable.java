90466413ea9cdeba9bfd303d69fa152dd849cd26
Amir Ebrahimi <amir.ebrahimi.tech@gmail.com>
2025-12-05 18:20:30 +0100

    Add Entite.java

---
+++ CaseIntraversable.java
@@ -0,0 +1,9 @@
+// la classe CaseIntraversable
+public class CaseIntraversable extends Case {
+    // les attributs
+
+    // le constructeur
+    public CaseIntraversable(int ligne, int colonne) {
+        super(ligne, colonne, new Void());
+    }
+}
---
+++ Entite.java
@@ -0,0 +1,10 @@
+// la classe abstrait Entite
+public abstract class Entite {
+    // les attributs
+    private int resistance;
+
+    // le constructeur
+    public Entite(int resistance) {
+        this.resistance = resistance;
+    }
+} 
